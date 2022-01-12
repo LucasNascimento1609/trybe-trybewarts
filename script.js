c89c37e3318c9794d7b1ce27db1b641d8457d96e
joaokruschewsky <joaokruschewsky@gmail.com>
2022-01-12 10:42:02 -0300

    Ajustando lint

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const getEmail = document.querySelector('#loginEmail')
-const getPass = document.querySelector('#loginPass')
-const sendBtn = document.querySelector('.getBtn')
-const btnEnviar = document.querySelector('#submit-btn')
-const checkBox = document.querySelector('#agreement')
+const getEmail = document.querySelector('#loginEmail');
+const getPass = document.querySelector('#loginPass');
+const sendBtn = document.querySelector('.getBtn');
+const btnEnviar = document.querySelector('#submit-btn');
+const checkBox = document.querySelector('#agreement');
 
 function loginTest() {
-  event.preventDefault()
-  if(getEmail.value == "tryber@teste.com" && getPass.value == '123456') {
-    alert('Olá, Tryber!')
+  preventDefault();
+  if (getEmail.value === 'tryber@teste.com' && getPass.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos.')
+    alert('Email ou senha inválidos.');
   }
 }
 
-sendBtn.addEventListener('click', loginTest)
+sendBtn.addEventListener('click', loginTest);
 
 function teste() {
-    btnEnviar.toggleAttribute('disabled')
+    btnEnviar.toggleAttribute('disabled');
 }
 
-checkBox.addEventListener('click', teste)
+checkBox.addEventListener('click', teste);
